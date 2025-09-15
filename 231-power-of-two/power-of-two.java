20675d857d38e52164a8dd8a9d9e79489b3650b7
Madiha-Saleem <madihasaleem9927@gmail.com>
2025-09-15 22:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (53.02%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0)
         return false;
@@ -7,5 +7,15 @@ class Solution {
             return true;
         }
         return false;
+    }
+}*/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0)
+        return false;
+        while(n%2==0)
+        n=n/2;
+        return n==1;
     }
 }
