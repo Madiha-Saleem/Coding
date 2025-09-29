514ed9922e46694ee5ee9a94c5e322f31be4758b
Madiha-Saleem <madihasaleem9927@gmail.com>
2025-09-29 23:19:21 +0530

    Time: 2 ms (60.75%) | Memory: 41.9 MB (47.61%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int i,j,l1,l2,s1=0,s2=0,ans;
+        l1=s.length();
+        l2=t.length();
+        char c1[]=new char[l1];
+        char c2[]=new char[l2];
+        for(i=0;i<l1;i++){
+            c1[i]=s.charAt(i);
+            s1=s1+(int)c1[i];
+        }
+        for(i=0;i<l2;i++){
+            c2[i]=t.charAt(i);
+            s2=s2+(int)c2[i];
+        }
+        ans=s2-s1;
+        return (char)ans;
+    }
+}
