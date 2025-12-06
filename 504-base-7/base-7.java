db44e8f5b81ace6b275b13aadce3e91275de430a
Madiha-Saleem <madihasaleem9927@gmail.com>
2025-12-06 10:06:52 +0530

    Time: 5 ms (13.14%) | Memory: 42.9 MB (9.83%) - LeetSync

---
+++ 504-base-7/base-7.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToBase7(int num) {
+        int r;
+        String s="";
+        if(num==0) return "0"; 
+        boolean isNeg=num<0;
+        num=Math.abs(num);
+        while(num!=0){
+            r=num%7;
+            num=num/7;
+            s=r+s;
+        }
+         if(isNeg) s="-"+s;
+        return s;
+    }
+}
