98ca822107718e3ca82bb209aad20e4ad1711be7
Madiha-Saleem <madihasaleem9927@gmail.com>
2026-01-05 22:13:45 +0530

    Time: 5 ms (88.90%) | Memory: 44 MB (87.15%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -2,6 +2,28 @@ class Solution {
     public String reverseWords(String s) {
         int i;
         char ch;
+        StringBuilder ch1=new StringBuilder();
+        StringBuilder s1 =new StringBuilder();
+        for(i=s.length()-1;i>=0;i--){
+            ch=s.charAt(i);
+            if(ch!=' '){
+                ch1.insert(0, ch);  // build word
+            }
+            else if(ch1.length()>0){
+                s1.append(ch1).append(' ');
+                ch1.setLength(0);  //clear word
+            }
+        }
+        if(ch1.length()>0)
+            s1.append(ch1);
+        return s1.toString().trim();
+    }
+}
+
+/*class Solution {
+    public String reverseWords(String s) {
+        int i;
+        char ch;
         String ch1="",s1="";
         for(i=s.length()-1;i>=0;i--){
             ch=s.charAt(i);
@@ -16,4 +38,4 @@ class Solution {
             s1=s1+ch1;
         return s1.trim();
     }
-}
+}*/
