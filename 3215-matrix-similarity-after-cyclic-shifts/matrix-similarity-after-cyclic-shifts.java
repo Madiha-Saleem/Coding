172e2cf8e2fbd99fdf387eeeb5ffdd232ba057d2
Madiha-Saleem <madihasaleem9927@gmail.com>
2026-03-27 10:58:37 +0530

    Time: 1 ms (99.47%) | Memory: 46.7 MB (95.20%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -3,6 +3,7 @@ class Solution {
         int m,n,i,j;
         m=mat.length;
         n=mat[0].length;
+        k=k%n;
         for(i=0;i<m;i++){
             for(j=0;j<n;j++){
                 if(mat[i][(j+k)%n]!=mat[i][j])
